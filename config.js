9163bef9b018981695622e7a975e50444a082135
rockm5245 <souravpramanik6300329@gmail.com>
2025-03-27 12:50:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,10 +9,10 @@ const DATABASE_URL =
   process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
 module.exports = {
   VERSION: require('./package.json').version,
-  SESSION_ID: (process.env.SESSION_ID || '').trim(),
+  SESSION_ID: (process.env.SESSION_ID || 'levanter_272f61638e33844c5688334944ac038908').trim(),
   DATABASE:
     DATABASE_URL === databasePath
-      ? new Sequelize({
+      ? new Sequelize({postgresql://
           dialect: 'sqlite',
           storage: DATABASE_URL,
           logging: false,
